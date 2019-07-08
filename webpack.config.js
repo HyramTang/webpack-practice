3753e709c79e3cb67c515de533e982f6eeed9f09
Hyram <hyram.tang@gmail.com>
2019-07-08 22:47:00 +0800

    style and css and less

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,9 @@ module.exports = {
     mode: 'production',
     module:{
         rules:[
-            {test:/\.js$/,use:'babel-loader'}
+            {test:/\.js$/,use:'babel-loader'},
+            {test:/\.css$/,use:['style-loader','css-loader']},
+            {test:/\.less$/,use:['style-loader','css-loader','less-loader']}
         ]
     }
 }
