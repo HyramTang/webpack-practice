89e90475981184c1aa2d9f1e73240d9247cf3072
Hyram <hyram.tang@gmail.com>
2019-07-09 22:27:06 +0800

    watch

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,16 @@
 const path = require('path');
 
 module.exports = {
+    //开启监听，默认 false
+    watch:true,
+    watchOptions: {
+        //忽略文件夹
+        ignored:/node_modules/,
+        //延迟 300 毫秒 build
+        aggregateTimeout:300,
+        //每秒询问 1000 次文件是否变化
+        poll:1000
+    },
     entry: {
         index:'./src/index.js',
         search:'./src/search.js'
