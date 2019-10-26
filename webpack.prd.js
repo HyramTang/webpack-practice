26b5e9a291bbcfb06a68eccac694ccef4cc91f74
Hyram <hyram.tang@gmail.com>
2019-10-26 23:40:57 +0800

    chapter 02: js&css&image&font hash

---
+++ webpack.prd.js
@@ -0,0 +1,54 @@
+'use strict'
+
+const path = require('path');
+const MiniCSSExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: {
+        index: './src/index.js',
+        search: './src/search.js'
+    },
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: '[name]_[chunkhash:8].js'
+    },
+    mode: 'production',
+    module: {
+        rules: [{
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCSSExtractPlugin.loader, 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: [MiniCSSExtractPlugin.loader, 'css-loader', 'less-loader']
+            },
+            {
+                test: /\.(png|jpg|gif|jpeg)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name]_[hash:8].[ext]'
+                    }
+                }]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [{
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name]_[hash:8].[ext]'
+                    }
+                }]
+            }
+        ]
+    },
+    plugins: [
+        new MiniCSSExtractPlugin({
+            filename: '[name]_[contenthash:8].css'
+        })
+    ]
+}
