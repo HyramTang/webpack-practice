a4b0c0cb68d27f0a2eadde3ce8e3acb615325fea
Hyram <hyram.tang@gmail.com>
2019-10-26 23:20:57 +0800

    chapter 02: watch&WDS

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,62 @@
 'use strict'
 
 const path = require('path');
+const webpack = require('webpack');
 
 module.exports = {
     //开启监听，默认 false
-    watch:true,
+    watch: false,
     watchOptions: {
         //忽略文件夹
-        ignored:/node_modules/,
+        ignored: /node_modules/,
         //延迟 300 毫秒 build
-        aggregateTimeout:300,
+        aggregateTimeout: 300,
         //每秒询问 1000 次文件是否变化
-        poll:1000
+        poll: 1000
     },
     entry: {
-        index:'./src/index.js',
-        search:'./src/search.js'
+        index: './src/index.js',
+        search: './src/search.js'
     },
     output: {
         path: path.join(__dirname, 'dist'),
         filename: '[name].js'
     },
-    mode: 'production',
-    module:{
-        rules:[
-            {test:/\.js$/,use:'babel-loader'},
-            {test:/\.css$/,use:['style-loader','css-loader']},
-            {test:/\.less$/,use:['style-loader','css-loader','less-loader']},
+    mode: 'development',
+    module: {
+        rules: [{
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
             // {test:/\.(png|jpg|gif|jpeg)$/,use:'file-loader'},
-            {test:/\.(png|jpg|gif|jpeg)$/,use:[{
-                loader:'url-loader',
-                options:{
-                    limit:20480
-                }
-            }]},
-            {test:/\.(woff|woff2|eot|ttf|otf)$/,use:'file-loader'}
+            {
+                test: /\.(png|jpg|gif|jpeg)$/,
+                use: [{
+                    loader: 'url-loader',
+                    options: {
+                        limit: 20480
+                    }
+                }]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: 'file-loader'
+            }
         ]
+    },
+    plugins: [
+        new webpack.HotModuleReplacementPlugin()
+    ],
+    devServer: {
+        contentBase: './dist',
+        hot: true
     }
 }
